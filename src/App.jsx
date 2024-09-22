86b1ae9a5c0e9724a828dd66b6936c8ef839cad7
Sergii Marchenko <marikmarchelo@gmail.com>
2024-09-22 20:40:05 +0300

    add commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import ContactForm from "./components/ContactForm/ContactForm";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactList from "./components/ContactList/ContactList";
 import "./App.css";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchContacts } from "./redux/contactsOps";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const dispatch = useDispatch();
 
-  return <></>;
-}
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <>
+      <div>
+        <h1 className="title">Phonebook</h1>
+        <ContactForm />
+        <SearchBox />
+        <ContactList />
+      </div>
+    </>
+  );
+};
 
 export default App;
